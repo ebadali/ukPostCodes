c0d9fc7503d0a331a7311f9da06f562b1b87d88d
ebad <ebadali@ebads-iMac.local>
2018-01-29 18:50:00 +0000

    midway Adding Test cases for validate and format functions

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
 
-
-from .context import sample
-
-
+from .context import postcodes
 import unittest
 
 
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
-    def test_thoughts(self):
-        self.assertIsNone(sample.hmm())
+    def test_validate_correct(self):
         
+        cases = ['AA9A 9AA',]
+        for case in cases:
+            self.assertEqual(postcodes.validate(case), True)
 
-    def test_validate_postcode(self):
-        print(sample.validate('AA9A 9AA'))
-        assert True
+    def test_validate_incorrect(self):
+        
+        cases = ['AA9A 9AA']
+        for case in cases:
+            self.assertEqual(postcodes.validate(case), True)            
+    def test_format_correct(self):
+        
+        cases = ['AA9A 9AA',]
+        for case in cases:
+            self.assertEqual(postcodes.validate(case), True)
+
+
+    def test_format_incorrect(self):
+        
+        cases = ['AA9A 9AA',]
+        for case in cases:
+            self.assertRaises(postcodes.format(case), True)
 
 
 if __name__ == '__main__':
